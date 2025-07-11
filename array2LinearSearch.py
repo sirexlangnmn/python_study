448061de093ec974be2bfb4b10351a27b5ddbd2c
Federex  A  Potolin <potolin.federex@gmail.com>
2025-07-11 23:40:34 +0800

    linear search

--- array2LinearSearch.py
+++ array2LinearSearch.py
@@ -4,16 +4,20 @@ def linear_search(arr, target):
             return i  # Return the index
     return -1  # Not found
 
+
 # Example 1
 students = ["Ana", "Ben", "Carla", "Dino", "Ella"]
 target_name = "Carla"
+
 
 # Example 2
 scores = [75, 89, 92, 67, 45, 58]
 target_score = 92
 
+
 result = linear_search(students, target_name)
 result2 = linear_search(scores, target_score)
+
 
 if result != -1:
     print(f"{target_name} found at index {result}")
